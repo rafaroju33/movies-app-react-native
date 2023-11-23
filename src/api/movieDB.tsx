0b8b1ea65224eee9ab67e7d1082abd0d8d288b06
Rafael <rafael@Rafaels-MacBook-Air.local>
2023-11-23 17:48:58 -0500

    update app screenshot.png

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '82e3ddd514216006888b0c9f1e2eb7cd', //Here use you own API Key of this API https://www.themoviedb.org/movie
+        api_key: '', //Here use you own API Key of this API https://www.themoviedb.org/movie
         lenguage: 'es-ES'
     }
 });
